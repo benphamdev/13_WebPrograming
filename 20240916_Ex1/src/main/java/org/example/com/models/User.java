d6ef808737ed5c664a4f79cbe9245beab398117d
benphamdev <duychien152003@gmail.com>
2024-09-19 16:53:10 +0700

    feat(views): Update CSS for forgot-password, login, register page
                  - Added animations for body, main, h1, label, input, button, and wrap elements.
                  - Changed color scheme to use Tomato and Indian Red instead of blue.
                  - Implemented gradient animation for button background.
                  - Added hover effect to underline login link.

--- 20240916_Ex1/src/main/java/org/example/com/models/User.java
+++ 20240916_Ex1/src/main/java/org/example/com/models/User.java
@@ -1,24 +1,27 @@
 package org.example.com.models;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.io.Serial;
 import java.io.Serializable;
+import java.util.Date;
 
 @Getter
 @Setter
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class User implements Serializable {
     @Serial
     private static final long serialVersionUID = 1L;
-    private String firstName;
-    private String lastName;
+
+    private int id;
     private String username;
     private String password;
-    private String address;
-    private String contact;
+    private String email;
+    private String fullName;
+    private String avatar;
+    private int roleId;
+    private String phone;
+    private Date createdDate;
 }
--- 20240916_Ex1/src/main/webapp/index.jsp
+++ 20240916_Ex1/src/main/webapp/index.jsp
@@ -2,11 +2,12 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <title>JSP - Hello World</title>
+    <title>JSP - Hello World</title>
 </head>
 <body>
-<h1><%= "Hello World!" %></h1>
-<br/>
-<a href="hello-servlet">Hello Servlet</a>
+    <%
+        response.sendRedirect(request.getContextPath() + "/waiting");
+    %>
+    <a href="auth/login">Login Page</a>
 </body>
 </html>
